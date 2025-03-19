7184e19c3a1027a7fda4cb7fdd2305dc796cea34
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:36:58 +0700

    “menambahkan class SimpleDataBase”

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Runtime.Intrinsics.X86;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static modul5_103022300014.Program;
 
 namespace modul5_103022300014
 {
@@ -8,9 +10,14 @@ namespace modul5_103022300014
         static void Main(string[] args)
         {
             PemrosesData pb = new PemrosesData();
+            SimpleDataBase<string> db= new SimpleDataBase<string>();
+            db.AddNewData("12");
+            db.AddNewData("30");
+            db.AddNewData("22");
             double d1 = 10, d2 = 30, d3 = 22;
             
             Console.WriteLine(pb.DapatkanNilaiTerbesar(d1, d2, d3));
+            db.PrintAllData();
         }
         public class PemrosesData
         {
@@ -31,6 +38,36 @@ namespace modul5_103022300014
                     jumlah = n3;
                 }
                 return jumlah;
+            }
+        }
+
+        public class SimpleDataBase<T>
+        {
+            private List<T> storedData;
+            private List<DateTime> inputDates;
+
+            public SimpleDataBase() { 
+                this.storedData = new List<T>();
+                this.inputDates = new List<DateTime>();
+            }
+
+            public void AddNewData(T storeData )
+            {
+                storedData.Add(storeData);
+                DateTime dateTime = DateTime.Now;
+                inputDates.Add(dateTime);
+            }
+
+            public void PrintAllData()
+            {
+                int i = 1;
+                foreach ( T storeData in storedData)
+                {
+                    Console.WriteLine($"Data {i} berisi {storeData}, yang " +
+                        $"tersimpan pada waktu UTC: {DateTime.Now}");
+                    i++;
+                }
+                
             }
         }
     }
